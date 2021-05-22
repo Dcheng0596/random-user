3d728ff8577f4d149241103dd85e8d7ffcc3aacd
Darien Cheng <cheng.darien@gmail.com>
2021-05-22 19:20:52 -0400

    Implement onTextSubmit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,13 +9,24 @@ export class AppComponent {
   public users: string = "";           
   public file: any;
   public textErrorMsg: string = "";
+  public fileErrorMsg: string = "";
+
 
   private errorMsgs: any = {
-    FILE_UPLOAD: "Error uploading file"
+    FILE_UPLOAD: "Error uploading file",
+    INVALID_JSON: "The JSON you entered is invalid"
   };
 
   public onTextSubmit(): void {
-    
+    let parsedJSON = this.parseJSON(this.users);
+
+    if(!parsedJSON) {
+      this.textErrorMsg = this.errorMsgs.INVALID_JSON;
+      return;
+    }
+
+    this.calcStats(parsedJSON);
+    return;
   };
 
   // Retrieve file from file input if exists
@@ -26,10 +37,26 @@ export class AppComponent {
     if (fileList) {
       this.file = fileList[0];
     }
-    this.textErrorMsg = this.errorMsgs.FILE_UPLOAD;
+    this.fileErrorMsg = this.errorMsgs.FILE_UPLOAD;
   };
 
   public onFileSubmit(): void {
     
   };
+
+  // Returns false if invalid JSON, else the parsed JSON
+  private parseJSON (jsonStr: string) {
+    try {
+      var res = JSON.parse(jsonStr);
+      if (res && typeof res === "object") {
+          return res;
+      }
+    }
+    catch (e) { }
+
+    return false;
+  };
+
+  private calcStats(jsonObj: object): void {
+  }
 }
