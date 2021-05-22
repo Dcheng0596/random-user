7da5cbef978b952e9cbe828287b697b18c3e8d9e
Darien Cheng <cheng.darien@gmail.com>
2021-05-22 16:58:29 -0400

    Create event handlers and bind them to input tags

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'random-user';
+  public users : string = "";
+  public file : any;
+
+  public onTextSubmit(): void {
+    
+  }
+
+  public onFileChange(event: Event): void {
+    let element: HTMLInputElement = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    let temp: any;
+
+    if (fileList) {
+      temp = fileList[0];
+    }
+
+    
+
+    console.log(this.file);
+    
+    
+  }
+
+  public onFileSubmit(): void {
+    
+  }
 }
