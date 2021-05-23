c0e6b07eb445c50b1542d565559ed05eef0c351c
Darien Cheng <cheng.darien@gmail.com>
2021-05-23 17:10:37 -0400

    Implement setTotalTracker and setStateMap
    Parse the users to obtian the total number of people and their genders
    as well as the number of peope in each state

--- client/src/app/random-user-stats.ts
+++ client/src/app/random-user-stats.ts
@@ -1,33 +1,63 @@
+interface PeopleTracker {
+    people: number,
+    male: number,
+    female: number
+}
+
 export class RandomUserStats {
     private users: any = {};
 
-    public people: number = 0;
-    public male: number = 0;
-    public female: number = 0;
+    public totalPeople: PeopleTracker;
 
-    private stateMap: Map<string, Object> = new Map();
+    private stateMap: Map<string, PeopleTracker>;
     private ageGroups: Object = {};
     
 
     constructor(users: any = {}) {
         this.users = users;
+        this.totalPeople = { people: 0, male: 0, female: 0 };
+        this.stateMap = new Map();
     }
 
     public calculateStatistics(users: any = this.users) {
-        let errMsg = " is not defined"
         try {
             if(!users.hasOwnProperty("results")) {
-                throw new ReferenceError("results" + errMsg);
+                throw new ReferenceError("results is not defined");
             }
             let results = this.users.results;
 
             for(let user of results) {
-                console.log(user.name.first);
-                
-            }
+                this.setTotalTracker(user);
+                this.setStateMap(user);
+            }            
         } catch (e) {
             throw e;     
         }
     }
 
+    private setTotalTracker(user: any): void {
+        if(!user.hasOwnProperty("gender")) {
+            throw new ReferenceError("gender is not defined");
+        }
+
+        this.totalPeople.people++;
+        user.gender == "male" ? this.totalPeople.male++ : this.totalPeople.female++;
+    }
+
+    private setStateMap(user: any) {
+        let state = user.location.state;
+        
+        if(state == undefined) {
+            throw new ReferenceError("state is not defined");
+        }
+
+        let tracker = this.stateMap.get(state);
+
+        if(!tracker) {
+            this.stateMap.set(state, { people: 0, male: 0, female: 0 });
+        } else {
+            tracker.people++;
+            user.gender == "male" ? tracker.male++ : tracker.female++;
+        }
+    }
 }
